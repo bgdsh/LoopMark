6b47286e5fb7251cc19b5964298ee0edff13b5c8
bgd <bgd_sh@hotmail.com>
2016-07-28 23:27:03 +0800

    remote method

--- common/models/bookmark.js
+++ common/models/bookmark.js
@@ -1,3 +1,26 @@
-module.exports = function(Bookmark) {
-
+module.exports = function (Bookmark) {
+    Bookmark.observe('before save', function updateTimestamp(ctx, next) {
+        if (ctx.instance) {
+            console.log(ctx.instance);
+            ctx.instance.updated = new Date();
+        } else {
+            console.log(ctx.data);
+            ctx.data.updated = new Date();
+        }
+        next();
+    });
+    Bookmark.pubilc=function getPublicBookmarks(page_index,cb) {
+        let pageDataCount=10;
+        let pageIndex= page_index || 1;
+        let skip = (pageIndex-1)*pageDataCount;
+        Bookmark.find({
+            where:{isPublic:true},
+            skip:skip,
+            limit:pageDataCoun
+        },cb);
+    }
+    Bookmark.remoteMethod('public',{
+        accepts:{arg:'page_index',type:'number'},
+        returns: {type:'array',root:'true'}
+    });
 };
--- server/boot/root.js
+++ server/boot/root.js
@@ -1,6 +1,5 @@
 module.exports = function(app) {
   app.enableAuth();
-  // Install a `/` route that returns server status
   var router = app.loopback.Router();
   router.get('/', app.loopback.status());
   app.use(router);
