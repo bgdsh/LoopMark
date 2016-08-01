9f215a0fbcac72b7dd733b882977d760468a25d4
bgd <bgd_sh@hotmail.com>
2016-08-01 10:37:24 +0800

    add acl

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -1,8 +1,8 @@
 module.exports = function (app) {
-    app.dataSources.mysqlDs.automigtate('CoffeeShop', function (err) {
-        if (err) {
-            throw err;
-        }
+    // app.dataSources.mysqlDs.automigtate('CoffeeShop', function (err) {
+    //     if (err) {
+    //         throw err;
+    //     }
         
-    });
+    // });
 };
